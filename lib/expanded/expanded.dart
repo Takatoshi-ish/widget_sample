6c2066f134d6be7c0b5c193d8111b720dc14cd1e
Takatoshi-ish <ishiguro.taka@gmail.com>
2022-09-17 03:54:47 +0900

    Expanded sample追加

---
+++ lib/expanded/expanded.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expanded Column Sample'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+              color: Colors.blue,
+              height: 100,
+              width: 100,
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.amber,
+                width: 100,
+              ),
+            ),
+            Container(
+              color: Colors.blue,
+              height: 100,
+              width: 100,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
