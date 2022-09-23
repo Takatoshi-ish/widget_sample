22e40ecd7585112f2ff053d061745f3dc005315f
Takatoshi-ish <ishiguro.taka@gmail.com>
2022-09-23 19:41:45 +0900

    alwaysIncludeSemantics 修正

--- lib/opacity/alwaysIncludeSemantics.dart
+++ lib/opacity/alwaysIncludeSemantics.dart
@@ -7,53 +7,29 @@ class OpacitySample extends StatelessWidget {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(50.0),
         child: Center(
           child: Column(
             children: [
-              const CustomBox(text: 'No Opacity'),
-              const SizedBox(height: 15.0),
-              Opacity(
-                opacity: 0.0,
-                alwaysIncludeSemantics: false,
-                child: Semantics(
-                  onTap: () {
-                    print("Opacity ボックス");
-                  },
-                  label: 'カウンター ボタン',
-                  child: Container(
-                    padding: const EdgeInsets.all(100),
-                    child: const Text('Hello '),
+              SizedBox(
+                width: 300,
+                height: 300,
+                child: Opacity(
+                  opacity: 0.0,
+                  alwaysIncludeSemantics: false,
+                  child: Semantics(
+                    onTap: () {
+                      print("Opacity ボックス");
+                    },
+                    label: 'alwaysIncludeSemantics true',
+                    child: Container(
+                      padding: const EdgeInsets.all(100),
+                      child: const Text('Hello Word'),
+                    ),
                   ),
                 ),
               ),
-              const SizedBox(height: 15.0),
-              const CustomBox(text: 'No Opacity'),
             ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class CustomBox extends StatelessWidget {
-  const CustomBox({super.key, this.text = ''});
-
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
-      height: 200.0,
-      width: 200.0,
-      child: Center(
-        child: Text(
-          text,
-          style: const TextStyle(
-            fontSize: 15.0,
-            color: Colors.white,
           ),
         ),
       ),
