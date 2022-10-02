e34839445a67bbf443ec5934f8089da24aa34e62
Takatoshi-ish <ishiguro.taka@gmail.com>
2022-10-02 20:32:11 +0900

    fab sample1追加

---
+++ lib/FloatingActionButton/fab_sample1.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class FabExample extends StatelessWidget {
+  const FabExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FloatingActionButton Sample'),
+      ),
+      body: const Center(child: Text('Press the button below!')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your onPressed code here!
+        },
+        backgroundColor: Colors.green,
+        child: const Icon(Icons.navigation),
+      ),
+    );
+  }
+}
