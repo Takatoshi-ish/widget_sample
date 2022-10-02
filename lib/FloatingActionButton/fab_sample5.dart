9c439c137007bd5303ed6f2d7c4c8ac3f1fff1e0
Takatoshi-ish <ishiguro.taka@gmail.com>
2022-10-03 00:20:18 +0900

    Flutter Action Button Sample4 追加

---
+++ lib/FloatingActionButton/fab_sample5.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class FabExample extends StatelessWidget {
+  const FabExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FloatingActionButton Sample'),
+      ),
+      body: const Center(child: Text('Press the button below!')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your onPressed code here!
+        },
+        backgroundColor: Colors.green,
+        child: const Icon(Icons.navigation),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.yellow,
+        child: Container(height: 50.0),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
