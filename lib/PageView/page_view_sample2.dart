b06018ca918871eedad30fbcff1230bc28b054c8
Takatoshi-ish <ishiguro.taka@gmail.com>
2022-10-04 04:46:13 +0900

    PageView 縦方向で実現

---
+++ lib/PageView/page_view_sample2.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class PageViewSample extends StatelessWidget {
+  const PageViewSample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final PageController controller = PageController();
+    return PageView(
+      /// [PageView.scrollDirection] defaults to [Axis.horizontal].
+      scrollDirection: Axis.vertical,
+      controller: controller,
+      children: const <Widget>[
+        Center(
+          child: Text('First Page'),
+        ),
+        Center(
+          child: Text('Second Page'),
+        ),
+        Center(
+          child: Text('Third Page'),
+        ),
+      ],
+    );
+  }
+}
