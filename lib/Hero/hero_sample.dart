0a1358c2fadbb2941ddad55b5dca71d36dfb14d8
Takatoshi-ish <ishiguro.taka@gmail.com>
2022-10-21 07:15:25 +0900

    Hero sample追加

---
+++ lib/Hero/hero_sample.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class HeroSample extends StatelessWidget {
+  const HeroSample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main Screen'),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return const DetailScreen();
+          }));
+        },
+        child: Hero(
+          tag: 'imageHero',
+          child: Image.network(
+            'https://picsum.photos/250?image=9',
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Center(
+          child: Hero(
+            tag: 'imageHero',
+            child: Image.network(
+              'https://picsum.photos/250?image=9',
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
