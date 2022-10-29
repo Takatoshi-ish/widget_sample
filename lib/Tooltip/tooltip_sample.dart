263a936ddabae25a5819a7cd8e3af5813dd0fafe
Takatoshi-ish <ishiguro.taka@gmail.com>
2022-10-29 19:17:23 +0900

    tooltip sampleコード追加

---
+++ lib/Tooltip/tooltip_sample.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TooltipSample extends StatelessWidget {
+  const TooltipSample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Tooltip(
+      message: 'I am a Tooltip',
+      child: Text('Hover over the text to show a tooltip.'),
+    );
+  }
+}
