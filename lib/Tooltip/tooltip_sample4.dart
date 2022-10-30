e08f6deeea09cc6206cbd7fd83dfc04515cb031d
Takatoshi-ish <ishiguro.taka@gmail.com>
2022-10-30 15:32:36 +0900

    tooltip sample4 追加

---
+++ lib/Tooltip/tooltip_sample4.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class TooltipSample extends StatelessWidget {
+  const TooltipSample({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    final GlobalKey<TooltipState> tooltipkey = GlobalKey<TooltipState>();
+
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: Center(
+        child: Tooltip(
+          // Provide a global key with the "TooltipState" type to show
+          // the tooltip manually when trigger mode is set to manual.
+          key: tooltipkey,
+          triggerMode: TooltipTriggerMode.manual,
+          showDuration: const Duration(seconds: 5),
+          message: 'I am a Tooltip',
+          child: const Text('Tap on the FAB'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          // Show Tooltip programmatically on button tap.
+          tooltipkey.currentState?.ensureTooltipVisible();
+        },
+        label: const Text('Show Tooltip'),
+      ),
+    );
+  }
+}
