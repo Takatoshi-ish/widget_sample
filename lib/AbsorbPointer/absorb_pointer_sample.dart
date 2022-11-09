a26320c3a6678239719b3558784d9a1d443b0522
Takatoshi-ish <ishiguro.taka@gmail.com>
2022-11-09 21:56:51 +0900

    AbsorbPointer sample 追加

---
+++ lib/AbsorbPointer/absorb_pointer_sample.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class AbsorbPointerSample extends StatelessWidget {
+  const AbsorbPointerSample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Stack(
+        alignment: AlignmentDirectional.center,
+        children: <Widget>[
+          SizedBox(
+            width: 200.0,
+            height: 100.0,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: null,
+            ),
+          ),
+          SizedBox(
+            width: 100.0,
+            height: 200.0,
+            child: AbsorbPointer(
+              absorbing: false,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue.shade200,
+                ),
+                onPressed: () {},
+                child: null,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
