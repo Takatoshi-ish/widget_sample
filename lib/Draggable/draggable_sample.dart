ad49aa8ca3cd81ec509bcde6483d63c1d8d03c9d
Takatoshi-ish <ishiguro.taka@gmail.com>
2022-12-05 08:11:30 +0900

    Draggable サンプル追加

---
+++ lib/Draggable/draggable_sample.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class DraggableSample extends StatefulWidget {
+  const DraggableSample({super.key});
+
+  @override
+  State<DraggableSample> createState() => _DraggableSampleState();
+}
+
+class _DraggableSampleState extends State<DraggableSample> {
+  int acceptedData = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Draggable<int>(
+            // Data is the value this Draggable stores.
+            data: 10,
+            feedback: Container(
+              color: Colors.deepOrange,
+              height: 100,
+              width: 100,
+              child: const Icon(Icons.directions_run),
+            ),
+            childWhenDragging: Container(
+              height: 100.0,
+              width: 100.0,
+              color: Colors.pinkAccent,
+              child: const Center(
+                child: Text('Child When Dragging'),
+              ),
+            ),
+            child: Container(
+              height: 100.0,
+              width: 100.0,
+              color: Colors.lightGreenAccent,
+              child: const Center(
+                child: Text('Draggable'),
+              ),
+            ),
+          ),
+          DragTarget<int>(
+            builder: (
+              BuildContext context,
+              List<dynamic> accepted,
+              List<dynamic> rejected,
+            ) {
+              return Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.cyan,
+                child: Center(
+                  child: Text('Value is updated to: $acceptedData'),
+                ),
+              );
+            },
+            onAccept: (int data) {
+              setState(() {
+                acceptedData += data;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
